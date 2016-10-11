e083c52e75d832f9968127ceff44fe9c333fed0b
syc <1577530083@qq.com>
2016-10-11 13:39:40 +0800

    实现分页指令逻辑部分

--- src/app/analysis/analysisCtrl.js
+++ src/app/analysis/analysisCtrl.js
@@ -2,5 +2,13 @@
 
 angular.module('app')
     .controller('analysisCtrl',function(){
-        
+        var ctrl=this;
+
+        ctrl.numPages=10;
+        ctrl.currentPage=1;
+
+        ctrl.selectPage=function(page){
+            alert(page);
+        }
+
 });
--- src/common/directive/pagination/pagination.js
+++ src/common/directive/pagination/pagination.js
@@ -6,12 +6,54 @@ angular.module('app')
             restrict:'E',
             scope:{
                 numPages:'=',
-                currentPage:'='
+                currentPage:'=',
+                onSelectPage:'&'
             },
             templateUrl:'common/directive/pagination/pagination.html',
             replace:true,
             link:function(scope){
+                scope.$watch('numPages', function(value) {
+                    scope.pages = [];
+                    for(var i=1;i<=value;i++) { scope.pages.push(i); }
+                    if ( scope.currentPage > value ) {
+                        scope.selectPage(value);
+                    }
+                });
 
+                scope.isActive = function(page) {
+                    return scope.currentPage === page;
+                };
+                scope.selectPage = function(page) {
+                    if (!scope.isActive(page)) {
+                        scope.currentPage = page;
+                    }
+                };
+
+                scope.noPrevious=function(){
+                    return scope.currentPage<=1;
+                };
+                scope.selectPrevious=function(){
+                    if ( !scope.noPrevious() ) {
+                        scope.selectPage(scope.currentPage-1);
+                    }
+                };
+
+                scope.noNext=function(){
+                    return scope.currentPage===scope.numPages;
+                };
+
+                scope.selectNext = function() {
+                    if ( !scope.noNext() ) {
+                        scope.selectPage(scope.currentPage+1);
+                    }
+                };
+
+                scope.selectPage = function(page) {
+                    if ( ! scope.isActive(page) ) {
+                        scope.currentPage = page;
+                        scope.onSelectPage({ page: page });
+                    }
+                };
             }
         }
     });
