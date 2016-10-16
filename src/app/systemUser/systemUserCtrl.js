b1e394c03dcf86c81479cdd98ff7ffaf92581825
syc <1577530083@qq.com>
2016-10-16 20:55:08 +0800

    增加勾选效果，弹出效果代码优化

--- src/app/systemUser/systemUserCtrl.js
+++ src/app/systemUser/systemUserCtrl.js
@@ -4,100 +4,7 @@ angular.module('app')
 	.controller('systemUserCtrl', ['$http', '$uibModal', function ($http, $uibModal) {
 		var ctrl = this;
 
-		ctrl.doAdd = function () {
-			var modalInstance = $uibModal.open({
-				animation: true,
-				ariaLabelledBy: 'modal-title',
-				ariaDescribedBy: 'modal-body',
-				templateUrl: "app/systemUser/addSystemUser.html",
-				controller: 'ModalInstanceCtrl',
-				controllerAs: 'ctrl',
-				resolve: {
-					result: function () {
-						return $http({
-							url: 'data/systemUser/power.json',
-							method: 'get'
-						});
-					}
-				}
-			});
-
-			modalInstance.result.then(function (selectedItem) {
-			}, function () {
-			});
-
-		};
-
-		ctrl.doEdit = function () {
-			var modalInstance = $uibModal.open({
-				animation: true,
-				ariaLabelledBy: 'modal-title',
-				ariaDescribedBy: 'modal-body',
-				templateUrl: "app/systemUser/addSystemUser.html",
-				controller: 'ModalInstanceCtrl',
-				controllerAs: 'ctrl',
-				resolve: {
-					result: function () {
-						return $http({
-							url: 'data/systemUser/power.json',
-							method: 'get'
-						});
-					}
-				}
-			});
-
-			modalInstance.result.then(function (selectedItem) {
-			}, function () {
-			});
-		};
-
-		ctrl.doGive = function () {
-			var modalInstance = $uibModal.open({
-				animation: true,
-				ariaLabelledBy: 'modal-title',
-				ariaDescribedBy: 'modal-body',
-				templateUrl: "app/systemUser/giveSystemUser.html",
-				controller: 'ModalInstanceCtrl',
-				controllerAs: 'ctrl',
-				size:'sm',
-				resolve: {
-					result: function () {
-						return $http({
-							url: 'data/systemUser/power.json',
-							method: 'get'
-						});
-					}
-				}
-			});
-
-			modalInstance.result.then(function (selectedItem) {
-			}, function () {
-
-			});
-		};
-
-		ctrl.doDelete = function () {
-			var modalInstance = $uibModal.open({
-				animation: true,
-				ariaLabelledBy: 'modal-title',
-				ariaDescribedBy: 'modal-body',
-				templateUrl: "app/systemUser/deleteSystemUser.html",
-				controller: 'ModalInstanceCtrl',
-				controllerAs: 'ctrl',
-				resolve: {
-					result: function () {
-						return $http({
-							url: 'data/systemUser/power.json',
-							method: 'get'
-						});
-					}
-				}
-			});
-
-			modalInstance.result.then(function (selectedItem) {
-			}, function () {
-			});
-		};
+		ctrl.checked=false;
 
 		$http({
 			url: 'data/systemUser/systemUser.json',
@@ -106,8 +13,72 @@ angular.module('app')
 			ctrl.systemUsers = response.data;
 		});
 
+		ctrl.onCheck=function(){
+			if(ctrl.checked){
+				angular.forEach(ctrl.systemUsers,function(user){
+					user.checked=true;
+				});
+			}else{
+				angular.forEach(ctrl.systemUsers,function(user){
+					user.checked=false;
+				})
+
+			}
+		};
+
+		ctrl.myModal={
+			animation: true,
+			ariaLabelledBy: 'modal-title',
+			ariaDescribedBy: 'modal-body',
+			templateUrl: "app/systemUser/addSystemUser.html",
+			controller: 'ModalInstanceCtrl',
+			controllerAs: 'ctrl',
+			resolve: {
+				result: function () {
+					return $http({
+						url: 'data/systemUser/power.json',
+						method: 'get'
+					});
+				}
+			}
+		};
 
 
+		ctrl.doAdd = function () {
+			ctrl.myModal.templateUrl="app/systemUser/addSystemUser.html";
+			var modalInstance = $uibModal.open(ctrl.myModal);
+
+			modalInstance.result.then(function (selectedItem) {
+			}, function () {
+			});
+
+		};
+
+		ctrl.doEdit = function () {
+			ctrl.myModal.templateUrl="app/systemUser/editSystemUser.html";
+			var modalInstance = $uibModal.open(ctrl.myModal);
+			modalInstance.result.then(function (selectedItem) {
+			}, function () {
+			});
+		};
+
+		ctrl.doGive = function () {
+			ctrl.myModal.templateUrl="app/systemUser/giveSystemUser.html";
+			ctrl.myModal.seze='sm';
+			var modalInstance = $uibModal.open(ctrl.myModal);
+			modalInstance.result.then(function (selectedItem) {
+			}, function () {
+
+			});
+		};
+
+		ctrl.doDelete = function () {
+			ctrl.myModal.templateUrl="app/systemUser/deleteSystemUser.html";
+			var modalInstance = $uibModal.open(ctrl.myModal);
+			modalInstance.result.then(function (selectedItem) {
+			}, function () {
+			});
+		};
 
 	}]).controller('ModalInstanceCtrl', function ($uibModalInstance,result) {
 		var ctrl = this;
